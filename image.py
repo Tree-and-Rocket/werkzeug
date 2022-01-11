2f2a7c3bc6300e9a9fcce37d528d559b8902e7c3
Konrad Hinkelmann <konrad.hinkelmann@gmail.com>
2022-01-11 20:04:35 +0100

    Update image.py

--- image.py
+++ image.py
@@ -1,5 +1,12 @@
-import os 
+import os
+import numpy as np
 from PIL import Image
 
 
-
+def get_sharpness(image_path):
+    # to grayscale
+    im = Image.open(in_image_path).convert('L')
+    array = np.asarray(im, dtype=np.int32)
+    gy, gx = np.gradient(array)
+    gnorm = np.sqrt(gx**2 + gy**2)
+    return np.average(gnorm)
