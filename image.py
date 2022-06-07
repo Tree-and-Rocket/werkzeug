d33730809e8b904bd93a81ec61da618d30a2cef0
daanLockhorst <daanlockhorst@hotmail.com>
2022-06-07 08:43:59 +0200

    Update image.py

--- image.py
+++ image.py
@@ -1,5 +1,8 @@
 import os
 import numpy as np
+import cv2
+import os
+from multiprocessing import Pool
 from PIL import Image
 
 
@@ -10,3 +13,17 @@ def get_sharpness(image_path):
     gy, gx = np.gradient(array)
     gnorm = np.sqrt(gx**2 + gy**2)
     return np.average(gnorm)
+
+
+def resize_save(url):
+
+    img = cv2.imread('images/' + url) 
+    img = cv2.resize(img, (1024, 1024))
+    cv2.imwrite("sphaeraIMGS/"  + url[:-4] + ".png", img)
+    
+    
+
+if __name__ == "__main__":
+    cpu_cores = 8
+    p = Pool(cpu_cores)
+    p.map(proc, os.listdir('images/'))
